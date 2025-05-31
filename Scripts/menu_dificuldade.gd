9a49effe744352c492e28803c61fbae2876e94e5
Mauricio Pollis <mauriciopollis@gmail.com>
2025-05-31 12:50:23 -0300

    ligando a scene dos nomes com a scene do tabuleiro

--- Scripts/menu_dificuldade.gd
+++ Scripts/menu_dificuldade.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var qtd_players = 5
-@onready var tabuleiro_scene = preload("res://Scenes/tabuleiro.tscn").instantiate()
+var tabuleiro_scene
 
 func _on_facil_pressed() -> void:
 	setup_Jogadores()
@@ -19,6 +19,7 @@ func _on_botao_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu_modo_de_jogo.tscn")
 	
 func setup_Jogadores() -> void:
+	tabuleiro_scene = preload("res://Scenes/tabuleiro.tscn").instantiate()
 	var rng = RandomNumberGenerator.new()
 	for i in range(qtd_players):
 		tabuleiro_scene.jogadores.append(Jogador.new("Jogador " + str(i + 1), Color(rng.randf(), rng.randf(), rng.randf())))
--- Scripts/menu_setup_jogadores.gd
+++ Scripts/menu_setup_jogadores.gd
@@ -1,9 +1,9 @@
 extends Control
 
-@onready var tabuleiro_scene = preload("res://Scenes/tabuleiro.tscn").instantiate()
+var tabuleiro_scene
 
 func _init() -> void:
-	pass
+	tabuleiro_scene = preload("res://Scenes/tabuleiro.tscn").instantiate()
 	
 func _ready() -> void:
 	pass
@@ -11,14 +11,34 @@ func _ready() -> void:
 func inicializar(qtdJogadores: int, qtdIAs: int) -> void:
 	for i in range(qtdJogadores):
 		var lineEdit = LineEdit.new()
+		lineEdit.custom_minimum_size = Vector2(0, 75)
+		lineEdit.placeholder_text = "Jogador" + str(i + 1)
 		$Panel/HBoxContainer/NomesJogadores.add_child(lineEdit)
 	for i in range(qtdIAs):
+		var lineEdit = LineEdit.new()
+		lineEdit.custom_minimum_size = Vector2(0, 75)
+		lineEdit.placeholder_text = "IA " + str(i + 1)
 		$Panel/HBoxContainer/NomesIAs.add_child(LineEdit.new())
 	
-func setup_Jogadores() -> void:
+	setup_jogadores()
+	
+func setup_jogadores() -> void:
 	var rng = RandomNumberGenerator.new()
 	for lineEdit in $Panel/HBoxContainer/NomesJogadores.get_children():
-		tabuleiro_scene.jogadoresReais.append(Jogador.new(lineEdit.text, Color(rng.randf(), rng.randf(), rng.randf())))
+		var nome: String
+		if lineEdit.text == "":
+			nome = lineEdit.placeholder_text
+		else:
+			nome = lineEdit.text
+		tabuleiro_scene.jogadoresReais.append(Jogador.new(nome, Color(rng.randf(), rng.randf(), rng.randf())))
 		
 	for lineEdit in $Panel/HBoxContainer/NomesIAs.get_children():
-		tabuleiro_scene.jogadoresIAs.append(Jogador.new(lineEdit.text, Color(rng.randf(), rng.randf(), rng.randf())))
+		var nome: String
+		if lineEdit.text == "":
+			nome = lineEdit.placeholder_text
+		else:
+			nome = lineEdit.text
+		tabuleiro_scene.jogadoresIA.append(Jogador.new(nome, Color(rng.randf(), rng.randf(), rng.randf())))
+
+func _on_botao_jogar_modo_setup_jogadores_pressed() -> void:
+	get_tree().root.add_child(tabuleiro_scene)
