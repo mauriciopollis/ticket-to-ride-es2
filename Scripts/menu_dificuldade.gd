7c6c48aa07dce5e1b70030747b51f1169a464732
Mauricio Pollis <mauriciopollis@gmail.com>
2025-05-31 13:27:22 -0300

    implementando o fluxo de escolha de nomes de jogadores para o modo solo

--- Scripts/menu_dificuldade.gd
+++ Scripts/menu_dificuldade.gd
@@ -1,25 +1,23 @@
 extends Control
 
 @onready var qtd_players = 5
-var tabuleiro_scene
+var setup_jogadores_scene
 
 func _on_facil_pressed() -> void:
 	setup_Jogadores()
-	get_tree().root.add_child(tabuleiro_scene)
+	get_tree().root.add_child(setup_jogadores_scene)
 
 func _on_medio_pressed() -> void:
 	setup_Jogadores()
-	get_tree().root.add_child(tabuleiro_scene)
+	get_tree().root.add_child(setup_jogadores_scene)
 
 func _on_dificil_pressed() -> void:
 	setup_Jogadores()
-	get_tree().root.add_child(tabuleiro_scene)
+	get_tree().root.add_child(setup_jogadores_scene)
 
 func _on_botao_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu_modo_de_jogo.tscn")
 	
 func setup_Jogadores() -> void:
-	tabuleiro_scene = preload("res://Scenes/tabuleiro.tscn").instantiate()
-	var rng = RandomNumberGenerator.new()
-	for i in range(qtd_players):
-		tabuleiro_scene.jogadores.append(Jogador.new("Jogador " + str(i + 1), Color(rng.randf(), rng.randf(), rng.randf())))
+	setup_jogadores_scene = preload("res://Scenes/menu_setup_jogadores.tscn").instantiate()
+	setup_jogadores_scene.inicializar(5, 0)
